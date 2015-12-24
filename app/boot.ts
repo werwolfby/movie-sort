1f102eee3a7423eca63c5f2189a4b1666d326c68
werwolf.by <werwolf.by@gmail.com>
2015-12-24 22:35:19 +0300

    feat: Add minimal AppComponent.

---
+++ app/boot.ts
@@ -0,0 +1,4 @@
+import {bootstrap} from "angular2/platform/browser";
+import {AppComponent} from "./root/app.component";
+
+bootstrap(AppComponent);
---
+++ app/root/app.component.ts
@@ -0,0 +1,8 @@
+import {Component} from "angular2/core";
+
+@Component({
+    selector: 'ms-app',
+    template: '<h1>Main Component</h1>'
+})
+export class AppComponent {    
+}
